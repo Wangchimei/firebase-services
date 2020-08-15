e481365a1e6988204f773b556f8308b91c374a56
Chi Mei Wang <kimmy0929@gmail.com>
2020-08-15 19:12:56 +0900

    Enabled Google as a login provider

--- authentication/auth.js
+++ authentication/auth.js
@@ -101,26 +101,31 @@ loginForm.addEventListener('submit', (e) => {
 });
 
 // LOG IN WITH PROVIDER (GOOGLE)
-var provider = new firebase.auth.GoogleAuthProvider();
-auth
-  .signInWithPopup(provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+var google = new firebase.auth.GoogleAuthProvider();
+const googleBtn = document.querySelector('.google-login');
+googleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth
+    .signInWithPopup(google)
+    .then((result) => {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      // console.log(token);
+      // console.log(user);
+
+      const modal = document.querySelector('#modal-signup');
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+      signupForm.querySelector('.error').innerHTML = '';
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      console.log(errorMessage);
+    });
+});
 
 // Create
 createForm.addEventListener('submit', (e) => {
