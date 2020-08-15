b6bf4fb7a26ea9727eb99f0e6e31c129e852859d
Chi Mei Wang <kimmy0929@gmail.com>
2020-08-15 18:24:25 +0900

    remove config

--- authentication/auth.js
+++ authentication/auth.js
@@ -100,6 +100,28 @@ loginForm.addEventListener('submit', (e) => {
     });
 });
 
+// LOG IN WITH PROVIDER (GOOGLE)
+var provider = new firebase.auth.GoogleAuthProvider();
+auth
+  .signInWithPopup(provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
 // Create
 createForm.addEventListener('submit', (e) => {
   e.preventDefault();
